9d6fb60f678cf2b8ee8f0a1dae8e713bf0fd1c05
onalante-msft <89409054+onalante-msft@users.noreply.github.com>
2022-08-03 15:24:07 -0700

    Test configuration fallback behavior

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+
+IConfigurationRoot configuration = new ConfigurationBuilder()
+    .AddJsonFile("config.json")
+    .AddEnvironmentVariables()
+    .Build();
+
+bool? first = configuration.GetValue<bool?>("FOO");
+bool? second = configuration.GetValue<bool?>("BAR");
+bool third = configuration.GetValue<bool>("BAZ", false);
+Console.WriteLine(first);
+Console.WriteLine(second);
+Console.WriteLine(third);
+Console.WriteLine(first ?? second ?? third);
